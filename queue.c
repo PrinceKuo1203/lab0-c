1f149aa3497d5ad44c4f6bc921bddc52e149d409
PrinceKuo <princekuo1203@gmail.com>
2020-06-19 08:57:22 +0800

    Initial commit

--- queue.c
+++ queue.c
@@ -13,7 +13,12 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if (q) {
+        q->head = q->tail = NULL;
+        q->size = 0;
+    } else
+        q = NULL;
+
     return q;
 }
 
@@ -21,6 +26,28 @@ queue_t *q_new()
 void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
+    list_ele_t *temp = NULL, *temp_prev = NULL;
+    // for (temp = q->head; temp && !temp->next; temp = temp->next) {
+    //    free(temp->value);
+    //}
+
+    // need complete code...to be continuous
+    while (q->head->next) {
+        temp = q->head;
+
+        // free from tail
+        while (temp->next) {
+            temp_prev = temp;
+            temp = temp->next;
+        }
+        free(temp->value);
+        free(temp->next);
+        temp_prev->next = NULL;
+    }
+
+    free(q->head->value);
+    free(q->head->next);
+
     /* Free queue structure */
     free(q);
 }
@@ -34,13 +61,41 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q)
+        return false;
+
     list_ele_t *newh;
+    char *str;
     /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    str = malloc(strlen(s) + 1);  // allocate space for sting
+
+    if (!newh || !str) {
+        if (newh)
+            free(newh);
+        if (str)
+            free(str);
+
+        return false;
+    }
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+    // update tail of queue
+    if (!q->tail && !q->head)
+        q->tail = newh;
+
     newh->next = q->head;
     q->head = newh;
+
+    // copy string and save point address to object
+    int str_len = strlen(s);
+    strncpy(str, s, str_len);
+    *(str + str_len) = 0;
+    newh->value = str;
+
+    // update queu count
+    q->size++;
+
     return true;
 }
 
@@ -56,7 +111,40 @@ bool q_insert_tail(queue_t *q, char *s)
     /* TODO: You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    if (!q)
+        return false;
+
+    list_ele_t *newh;
+    char *str;
+    /* TODO: What should you do if the q is NULL? */
+    newh = malloc(sizeof(list_ele_t));
+    str = malloc(sizeof(char) * strlen(s));  // allocate space for sting
+
+    if (!newh || !str) {
+        if (newh)
+            free(newh);
+        if (str)
+            free(str);
+
+        return false;
+    }
+    /* Don't forget to allocate space for the string and copy it */
+    /* What if either call to malloc returns NULL? */
+    // update tail of queue
+    if (!q->tail && !q->head)
+        q->head = newh;
+
+    newh->next = q->tail;
+    q->tail = newh;
+
+    // copy string and save point address to object
+    strncpy(str, s, strlen(s));
+    newh->value = str;
+
+    // update queu count
+    q->size++;
+
+    return true;
 }
 
 /*
@@ -71,7 +159,25 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* TODO: You need to fix up this code. */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q)
+        return false;
+
+    // save q->head
+    list_ele_t *temp = q->head;
+
+    // assign new head
     q->head = q->head->next;
+
+    bufsize = strlen(temp->value);
+    strncpy(sp, temp->value, bufsize);
+    *(sp + bufsize) = 0;
+
+    // free old head mempry
+    free(temp->value);
+    free(temp);
+
+    q->size--;
+
     return true;
 }
 
